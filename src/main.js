31d77e8ad1f1bf9d4d47723e048613932348737d
libin <leebin66@126.com>
2021-06-16 21:11:16 +0800

    图片懒加载功能

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// import lazyPlugin from 'vue3-lazy';
+import lazyPlugin from "vue3-lazy"; // 图片懒加载插件
 
 // 引入全局样式文件
 import "@/assets/scss/index.scss";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(lazyPlugin, {
+    location: require("@/assets/images/default.png"),
+  })
+  .mount("#app");
 
 // createApp(App).use(store).use(router).use(lazyPlugin, {
 //   loading: require('@/assets/images/default.png')
